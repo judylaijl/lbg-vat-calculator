5688b25b60e5b29a3c8397ab4c22d81ac041e420
Judy <Judy.lai.jl@gmail.com>
2024-09-06 11:18:57 +0100

    add some dropdown

--- src/VatRateField.jsx
+++ src/VatRateField.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import "bootstrap/scss/bootstrap"
 
 const VatRateField = (props) => {
 
@@ -21,5 +22,16 @@ const VatRateField = (props) => {
         </div>
     );
 }
+
+<div class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown
+  </button>
+  <ul class="dropdown-menu">
+    <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+    <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+    <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+  </ul>
+</div>
 
 export default VatRateField;
